aedb55533c5dcef1ca854d7fbfa8e58c08fdac8d
Ankush Pandit <ankushpandit2003@gmail.com>
2026-05-16 22:35:42 +0530

    Updated production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://aromalux-2snl.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
