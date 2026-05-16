65b1342d299f8c63e281f897a455ac23ada538e6
Ankush Pandit <ankushpandit2003@gmail.com>
2026-05-17 01:00:46 +0530

    Fix Vercel SPA routing

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,15 +2,10 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
     },
   });
 
